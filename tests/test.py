60fb16c9013452da149fa1f107795cc2828a7bcd
Peterson <pkbusinesstrader@gmail.com>
2025-09-27 13:56:07 +0300

    add requirements.txt

---
+++ tests/test.py
@@ -0,0 +1,25 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from BackpropX.engine import Value
+from BackpropX.draw import draw_dot
+
+
+# sys.path.append(os.getcwd())
+# print(sys.path)
+
+# a = Value(2);a.label = "a"
+# b = Value(3);b.label = 'b'
+# c = a + b; c.label = 'c'
+# c.backward()
+# draw_dot(c)
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a*b; e.label = 'e'
+d = e + c; d.label = 'd'
+f = Value(-2.0, label='f')
+l = d * f +7; l.label = 'L'
+g = l.tanh(); g.label = 'g'
+g.backward()
+draw_dot(g)
